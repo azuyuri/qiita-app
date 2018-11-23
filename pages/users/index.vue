8137c816a785856454694f30b9ce92d0a4c33ff1
azuyuri <tamondojo@gmail.com>
2018-11-23 12:15:57 +0900

    feat: Change _id.vue

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,19 +1,10 @@
 <template>
-    <div>
-      <!--  -->
-    </div>
+  <div>
+    <p>
+      /users/index.vue
+    </p>
+  </div>
 </template>
-
-<script>
-export default {
-  async mounted() {
-    console.log(
-      JSON.stringify(await
-      this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, ' ')
-    )
-  }
-}
-</script>
 
 
 
